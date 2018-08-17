a0b15c94106e1819520414584b3daea789bddce0
Joao Carvalho <contato.jccarvalho@hotmail.com>
2018-08-17 20:12:52 -0300

    [ADD] Adicionando biblioteca mdbootstrap ao projeto

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'mdbreact/dist/css/mdb.css';
 
 class App extends Component {
   render() {
@@ -8,11 +11,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title"></h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <a className="btn btn-success waves-effect waves-light" href="#" target="_blank">Teste</a>
       </div>
     );
   }
